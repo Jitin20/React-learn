dff82d43588989abc5bfcd2cc9ddc5a39b68159d
Jitin Sharma <Jitin.Sharma@lntinfotech.com>
2022-12-02 07:41:20 +0530

    created react app Tried with state and some DOM Events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react"
 
 function App() {
+  // function greeting(name) {
+    
+  //   alert(`Hello, ${name}`);
+  // }
+  
+  // function processUserInput(callback) {
+  //   const name = prompt("Please enter your name.");
+  //   callback(name);
+  // }
+  
+  // processUserInput(greeting);
+  //Example of Callback functions.
+
+  const [userInput, setUserInput] = useState('')
+  const inputChangeHandler = (event)=>{
+    setUserInput(event.target.value)
+  }
+
+  function handleClick(e)
+  {
+    console.log(e.target)
+  }
+
+  // function handleCopy(){
+  //   alert("Don't copy, be your own.")
+  // }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <input type="text" name="name" 
+      onChange={inputChangeHandler} 
+      value = {userInput}/>
+<button onClick={handleClick}>Click Me</button>
+<p onCopy={handleCopy}>HEllo THere</p>
       </header>
     </div>
   );
